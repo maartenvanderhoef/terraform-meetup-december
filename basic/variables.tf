805876dace2fcf103d82aff164a4a1805abfa031
maartenvanderhoef <maartenvanderhoef@Maartens-MacBook-Pro.local>
2016-12-15 19:45:38 +0100

    adding key

--- basic/variables.tf
+++ basic/variables.tf
@@ -1,6 +1,13 @@
 variable "region" {
   description = "The AWS region"
 }
+variable "access_key" {
+  description = "The AWS Access key"
+}
+
+variable "secret_key" {
+  description = "The AWS secret key"
+}
 
 variable "shortname" {
   description = "The name for everything related to this setup"
